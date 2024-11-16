a89a2ecaea6c1746adec7726776eb8556264ea04
Manorath Bhatt <109541386+manibhatt18@users.noreply.github.com>
2024-11-16 03:14:55 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14566
+// const is used for declaring a constant
+let accountEmail = "mani@gmail.com"
+
+var accountPassword = "thebigone8"
+// var has scope problem, so it isn't used commonly nowadays, only let is used these days
+
+accountCity = "ktm" // this method of declaring a variable isn't good for programming practice
+
+// accountId = 4 // not allowed since accountId is a constant
+
+let accountState
+ console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
