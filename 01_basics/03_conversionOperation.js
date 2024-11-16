0bfe5094bbc78cd7defef7fae42fb7e198be807a
Manorath Bhatt <109541386+manibhatt18@users.noreply.github.com>
2024-11-16 03:46:50 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = 33
+let count = "45"
+console.log(typeof score)
+console.log(typeof count)
+
+let varchange = Number(count)
+console.log(typeof varchange)
+
+// "33" -> 33
+// "abc33" -> NaN (not a number)
+// true -> 1; false -> 0;
+
+
+// number to boolean conversion
+
+let isLoggedin = 1
+ let booleanLog = Boolean(isLoggedin)
+console.log(booleanLog)
+
+// 1 -> true, 0 -> false
+// "" -> false
+// "anyvalue" -> true
+
+// number to string conversion
+
+let anyNum = 40
+let stringNum = String(anyNum)
+console.log(stringNum)
+console.log(typeof stringNum)
