217aecadcc544ced27879c255ddb5dc36013ede9
Manorath Bhatt <109541386+manibhatt18@users.noreply.github.com>
2024-11-16 10:01:19 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,8 @@
+// javascript allows comparison of different data types
+
+console.log(2>3)  // it's op is false
+console.log("2"<4) // it's op is true
+
+// to avoid the above scenario we use === // === is used for strict check, it not only compares but also checks the data type
+
+console.log("5"===4)
