d896827766fb29fbc5f426edb465e1e6c6922332
Manorath Bhatt <109541386+manibhatt18@users.noreply.github.com>
2024-11-16 03:14:28 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict"; /*treat all code as newer version of javascript, although it isn't necessary to write this since the code is
+usually treated as newer version of js*/
+
+// alert(4+4) we are using node js not browser, so the output won't be 8
+ 
+// bigInt - used for very large number
+// string - " "
+// null -> standalone value
+// undefined - value hasn't been assigned yet
+// symbol - used for uniqueness, heavily used by application like figma
+
+//object
