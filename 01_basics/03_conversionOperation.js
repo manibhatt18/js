5f57acbc09a785e3fc927a52c430eaadf3f9ff16
Manorath Bhatt <109541386+manibhatt18@users.noreply.github.com>
2024-11-16 10:01:37 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -21,9 +21,25 @@ console.log(booleanLog)
 // "" -> false
 // "anyvalue" -> true
 
-// number to string conversion
+// // number to string conversion
 
-let anyNum = 40
-let stringNum = String(anyNum)
-console.log(stringNum)
-console.log(typeof stringNum)
+// let anyNum = 40
+// let stringNum = String(anyNum)
+// console.log(stringNum)
+// console.log(typeof stringNum)
+
+
+// //  operations //
+
+// console.log(4**4) // double * acts as an exponent
+
+// console.log(1+4+"4") // it's output will be 54
+// console.log("4"+1+3) // it's output will be 423 since the first element 4 is string, 1 and 3 are also treated as string
+
+// console.log(+true) // its output will be 1
+// console.log(+"") // its output will be 0
+
+let x=5;
+let y=x++
+console.log(x)
+console.log(y)
