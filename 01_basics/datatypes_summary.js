6ad2ecdc35ecfd8684648d4d9375066a4d1afaf0
Manorath Bhatt <109541386+manibhatt18@users.noreply.github.com>
2024-11-17 15:27:18 +0000

    summary plus stack and heap

--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -12,3 +12,28 @@
        Arrays  =>  object
        Function  =>  function
        Object  =>  object */
+
+// stack is used for primitive data types
+
+// heap is used for non primitive data types
+
+let channelName = "sportshala"
+
+let Name = channelName
+
+Name = "mani_things"
+
+console.log(channelName)
+console.log(Name)
+
+let userOne ={
+       email: "mani@google.com",
+       esewa: "12345"
+}
+
+let userTwo= userOne
+
+userTwo.email="doctor@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
